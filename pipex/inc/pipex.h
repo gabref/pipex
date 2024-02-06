435d121c705147ad890d690ef9e4c683e982404b
gabref <gabrielfranzer@gmail.com>
2024-02-06 15:58:00 +0100

    initial files for pipex

---
+++ pipex/inc/pipex.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: galves-f <galves-f@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 14:55:45 by galves-f          #+#    #+#             */
+/*   Updated: 2024/02/06 14:58:26 by galves-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+char	**split(char *str, char sep);
+
+#endif
---
+++ pipex/src/main.c
@@ -0,0 +1,7 @@
+#include "../inc/pipex.h"
+
+int main(void)
+{
+	printf("ciaoo\n");
+	return (0);
+}
